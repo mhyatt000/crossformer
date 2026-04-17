2a287d0278ba25df710db3d5dececf4a758d2a7c
matt <mhyatt@luc.edu>
2026-04-17 12:02:40 -0500

    remnant api fixes after bump: jax cache

--- crossformer/utils/jax_utils.py
+++ crossformer/utils/jax_utils.py
@@ -153,6 +153,6 @@ def initialize_compilation_cache(
     cache_dir=os.path.expanduser("~/.jax_compilation_cache"),
 ):
     """Initializes the Jax persistent compilation cache."""
-    compilation_cache.initialize_cache(cache_dir)
+    compilation_cache.set_cache_dir(cache_dir)
     for logger in [logging.getLogger(name) for name in logging.root.manager.loggerDict]:
         logger.addFilter(lambda record: "Not writing persistent cache entry for" not in record.getMessage())
